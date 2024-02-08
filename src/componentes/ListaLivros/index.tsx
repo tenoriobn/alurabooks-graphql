15c301bda26cbe262c158bc47064c2fa034d89ec
Bruno Tenório <tenoriobn@gmail.com>
2024-02-08 15:17:45 -0300

    primeira consulta com graphql

--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -1,19 +1,36 @@
+import { gql, useQuery } from "@apollo/client"
 import { ICategoria } from "../../interfaces/ICategoria"
-import { useQuery } from '@tanstack/react-query'
-import { obterProdutosDaCategoria } from "../../http"
 import CardLivro from "../CardLivro"
 
 import './ListaLivros.css'
+import { ILivro } from "../../interfaces/ILivro"
 
 interface ListaLivrosProps {
     categoria: ICategoria
 }
 
+const OBTER_LIVROS = gql`
+    query ObterLivros {
+        livros {
+            id
+            slug
+            titulo
+            imagemCapa
+            opcoesCompra {
+                id
+                preco
+            }
+        }
+    }
+`;
+
 const ListaLivros = ({ categoria }: ListaLivrosProps) => {
 
-    const { data: produtos } = useQuery(['buscaLivrosPorCategoria', categoria], () => obterProdutosDaCategoria(categoria))
+    const { data } = useQuery<{ livros: ILivro[] }>(OBTER_LIVROS)
+
+    // const { data: produtos } = useQuery(['buscaLivrosPorCategoria', categoria], () => obterProdutosDaCategoria(categoria))
     return <section className="livros">
-        {produtos?.map(livro => <CardLivro livro={livro} key={livro.id} />)}
+        {data?.livros.map(livro => <CardLivro livro={livro} key={livro.id} />)}
     </section>
 }
 
