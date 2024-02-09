876e597da93ca79348a55610c7754baad9e69e53
Bruno Tenório <tenoriobn@gmail.com>
2024-02-09 15:15:01 -0300

    criando campo de busca

--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -4,6 +4,8 @@ import CardLivro from "../CardLivro"
 
 import './ListaLivros.css'
 import { ILivro } from "../../interfaces/ILivro"
+import { AbBotao, AbCampoTexto } from "ds-alurabooks"
+import { useState } from "react"
 
 interface ListaLivrosProps {
     categoria: ICategoria
@@ -26,6 +28,8 @@ const OBTER_LIVROS = gql`
 
 const ListaLivros = ({ categoria }: ListaLivrosProps) => {
 
+    const [ textoBusca, setTextoBusca ] = useState('');
+
     const { data } = useQuery<{ livros: ILivro[] }>(OBTER_LIVROS, {
         variables: {
             categoriaId: categoria.id
@@ -33,9 +37,20 @@ const ListaLivros = ({ categoria }: ListaLivrosProps) => {
     })
 
     // const { data: produtos } = useQuery(['buscaLivrosPorCategoria', categoria], () => obterProdutosDaCategoria(categoria))
-    return <section className="livros">
-        {data?.livros.map(livro => <CardLivro livro={livro} key={livro.id} />)}
-    </section>
+    return (
+        <section>
+            <form style={{ maxWidth: '80%', margin: '0 auto', textAlign: 'center' }}>
+                <AbCampoTexto value={textoBusca} onChange={setTextoBusca} placeholder="Digite o título" />
+                <div style={{ marginTop: '16px' }}>
+                    <AbBotao texto="Buscar" />
+                </div>
+            </form>
+
+            <div className="livros">
+                {data?.livros.map(livro => <CardLivro livro={livro} key={livro.id} />)}
+            </div>
+        </section>
+    );
 }
 
 export default ListaLivros
